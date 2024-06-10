dfa0f8b4d3b044bf96fc60408ea3aea60e03b48c
R533-Code <raphaeldibnehmepc@gmail.com>
2024-06-10 23:57:59 +0200

    Added intrinsic_wakeall

--- src/asm/intrinsics.h
+++ src/asm/intrinsics.h
@@ -5,9 +5,24 @@
 
 FORCE_INLINE
 /// @brief Expands to a 'wfe' instruction.
+/// This is usually used inside of busy spinlock loops, to
+/// put the core in a low-power standby state.
+/// This is usually paired with 'intrinsic_wakeall' in
+/// the thread that acquire the spinlock.
 void intrinsic_pause()
 {
   asm ("wfe");
+}
+
+FORCE_INLINE
+/// @brief Expands to a 'sev' instruction.
+/// This is usually used after releasing a spinlock, to
+/// wake the cores that are in low-power standby state.
+/// This is usually paired with 'instrinsic_pause' in
+/// the threads that acquire the spinlock.
+void intrinsic_wakeall()
+{
+  asm ("sev");
 }
 
 #endif // !HG_INSTRINSICS
