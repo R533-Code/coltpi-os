2afae18f66f66cd0afe2d964fa4c41012a6fab16
R533-Code <raphaeldibnehmepc@gmail.com>
2024-06-11 00:31:22 +0200

    Included atomic_int

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "common/typedef.h"
 #include "boot/constants.h"
 #include "common/memop.h"
-#include "atomic/atomic_flag.h"
+#include "atomic/atomic_int.h"
 
 /// @brief The first function called by the bootloader
 void kernel_main()
