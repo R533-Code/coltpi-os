5bfbbf31a6ed0bbf722c0d87352f71a520f5bd48
R533-Code <raphaeldibnehmepc@gmail.com>
2024-06-11 15:44:44 +0200

    Added instrinsic_break helpers

--- src/asm/intrinsics.h
+++ src/asm/intrinsics.h
@@ -25,4 +25,26 @@ void intrinsic_wakeall()
   asm ("sev");
 }
 
+/// @brief Expands to an invalid instruction
+void intrinsic_und()
+{
+  asm volatile (".inst 0xe7f001f0");
+}
+
+#define __ASM_BLOCK(value) asm (value)
+/// @brief Expands to a 'brk' instruction.
+/// The macro is written in this clumsy way because
+/// otherwise syntax highlighting acts weirdly.
+#define intrinsic_break(u16) __ASM_BLOCK("brk" "#" STRINGIZE(u16))
+
+NEVER_INLINE
+/// @brief Expands to intrinsic_break(0xcfcf)
+/// This function is specifically called for 
+/// assertion failures in the kernel, as gdb
+/// will insert a breakpoint for this function.
+void intrinsic_break_0xcfcf(const char msg[], const char fn[])
+{
+  intrinsic_break(0xcfcf);
+}
+
 #endif // !__HG_INTRINSICS
