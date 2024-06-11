5c2fe4a87fbda413208404464fba177ace9fe8b2
R533-Code <raphaeldibnehmepc@gmail.com>
2024-06-11 15:45:27 +0200

    Added assert_true macro

---
+++ src/common/assert.h
@@ -0,0 +1,19 @@
+#ifndef __HG_ASSERT
+#define __HG_ASSERT
+
+#include "common/macro.h"
+#include "asm/intrinsics.h"
+ 
+#ifndef COLTPI_DEBUG
+  /// @brief Check if all conditions evaluates to true, else tries
+  ///        to breakpoint with information 'msg'.
+  #define assert_true(msg, bool, ...) do {} while(0)
+#else
+  #define __ASSERT_TRUE_AND_EXPAND(arg) && arg
+  #define assert_true(msg, bool, ...) do { \
+    if (!(bool FOR_EACH(__ASSERT_TRUE_AND_EXPAND, __VA_ARGS__))) { \
+      intrinsic_break_0xcfcf("Line " STRINGIZE(__LINE__)  ": " msg, __PRETTY_FUNCTION__); \
+    } } while (0)
+#endif
+
+#endif // !__HG_ASSERT
